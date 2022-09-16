0d1d411181b89830a34ff45917df3f2cbf7d2736
Nicolas Araya <naraya2018@inf.udec.cl>
2022-09-15 22:53:04 -0300

    Update count.cpp

--- counting/count.cpp
+++ counting/count.cpp
@@ -10,48 +10,31 @@ const unsigned char k = 31;
 
 
 int main(){
-	ifstream file;
+    ifstream file;
     file.open(pathFile);
     string line; 
     map<string, int> mp; 
-
     uint64_t count = 0;
     uint64_t countKmer = 0;
     while(file >> line){
         if(count++ % 100000==0) cout << count << endl;
-        if(line[0] != 'A' && line[0] != 'a' && line[0] != 'T' && line[0] != 't' && line[0] != 'C' && line[0] != 'c' && line[0] != 'G' && line[0] != 'g') continue; //linea no valida
-        else if(line[1] != 'A' && line[1] != 'a' && line[1] != 'T' && line[1] != 't' && line[1] != 'C' && line[1] != 'c' && line[1] != 'G' && line[1] != 'g') continue; //linea no valida
+        for(size_t i = 0; i < line.size() - k; i++){
+            string kmer; 
+            for(size_t j = 0; j  < k; j++){
+               kmer += line[i+j];
+            }   
 
-        else{ //linea valida
-            if(line.size() <= k) {
-                continue;
+            if(mp.count(kmer) > 0){ //si existe
+                //cout << kmer << " repetido " << countKmer << endl;
             }
-            //cout << "line: " << line << endl;
-            #pragma omp parallel for
-            for(size_t i = 0; i < line.size() - k; i++){
-                string kmer; 
-                for(size_t j = 0; j  < k; j++){
-                   kmer += line[i+j];
-                }   
-
-                if(mp.count(kmer) > 0){ //si existe
-                    //cout << kmer << " repetido " << countKmer << endl;
-                }
-                else{
-                    mp.insert(make_pair(kmer, 1));
-                    countKmer++;
-                    //cout << kmer << "nuevo" << countKmer << endl;
-                    
-                }
-                //cout << "k-mer " << i << ": " << kmer << endl;
-                kmer.clear();
+            else{
+                mp.insert(make_pair(kmer, 1));
+                countKmer++;
+                
             }
-            //cout << "nueva linea" << endl;
-       }
+            kmer.clear();
+        }
     }
-
     cout << "Cardinalidad: " << countKmer << endl;
-
-	
 	return 0;
 }
