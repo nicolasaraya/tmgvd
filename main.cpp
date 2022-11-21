d6e1a3bcccf25fd789fcf2ef9b1f0deb0106387f
Nicolas Araya <naraya2018@inf.udec.cl>
2022-11-20 23:16:04 -0300

    a

--- main.cpp
+++ main.cpp
@@ -12,17 +12,6 @@ const unsigned char k = 31;
 const string pathFile = "./data/file.fna";
 
 
-double JaccardHLL(HLL* hll1, HLL* hll2){
-    double A = hll1->estimate(); 
-    double B = hll2->estimate();
-    hll1->unionHLL(hll2->getSketch()); 
-    double AuB = hll1->estimate();
-    double AnB = A + B - AuB; 
-    double jac = AnB/AuB;  
-    return jac; 
-}
-
-
 int main(int argc, char const *argv[]){
     /*
     HLL* h;
@@ -36,13 +25,14 @@ int main(int argc, char const *argv[]){
     return 0;
 
     */
-
+    /*
     sdsl::csa_wt<> fm_index;
     sdsl::construct_im(fm_index, "mississippi!", 1);
     cout << "'si' occurs " << count(fm_index,"si") << " times.\n";
     sdsl::store_to_file(fm_index,"fm_index-file.sdsl");
     ofstream out("fm_index-file.sdsl.html");
     sdsl::write_structure<sdsl::HTML_FORMAT>(fm_index,out);
+    */
     
 }
 
